4fb92278699287046a6de4b9a6a073a728692744
RainbowWellington <hello@rainbowwellington.org.nz>
2026-06-11 02:17:27 +1200

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -2,11 +2,17 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import { neon } from '@neondatabase/serverless'
 import * as schema from './schema.js'
 
-const connectionString = process.env.NITRO_POSTGRES_URL || process.env.POSTGRES_URL
-
-if (!connectionString) {
-  throw new Error('No database connection string found.')
+function getDb() {
+  const connectionString = process.env.NITRO_POSTGRES_URL || process.env.POSTGRES_URL
+  if (!connectionString) {
+    throw new Error('No database connection string found.')
+  }
+  const sql = neon(connectionString)
+  return drizzle(sql, { schema })
 }
 
-const sql = neon(connectionString)
-export const db = drizzle(sql, { schema })
+export const db = new Proxy({} as ReturnType<typeof getDb>, {
+  get(_target, prop) {
+    return getDb()[prop as keyof ReturnType<typeof getDb>]
+  }
+})
